2f7b325db222fe337b77e982a23ddcd8ab718709
Henrique Kirch Heck <86362827+henriquekirchheck@users.noreply.github.com>
2024-08-01 16:21:41 -0300

    Thu Aug  1 04:21:41 PM -03 2024

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,29 @@
 import { defineConfig } from "vite";
+import { glob } from "glob";
+
+const input = (await glob("src/**/*.html"))
+	.map((path) => {
+		return [path.replace(/^src\//, "").replace(/\.html$/, ""), path] as const;
+	})
+	.reduce(
+		(map, keyValue) => {
+			map[keyValue[0]] = keyValue[1];
+			return map;
+		},
+		{} as { [entryAlias: string]: string },
+	);
 
 export default defineConfig({
-  server: {
-    open: "/index.html",
-  },
-  build: {
-    outDir: "../dist",
-    emptyOutDir: true,
-  },
-  css: {
-    transformer: "lightningcss",
-  },
-  root: "src",
+	server: {
+		open: "/index.html",
+	},
+	build: {
+		outDir: "../dist",
+		emptyOutDir: true,
+		rollupOptions: { input },
+	},
+	css: {
+		transformer: "lightningcss",
+	},
+	root: "src",
 });
