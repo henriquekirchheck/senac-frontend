4ec0d1405d116f8afc5179f361d54fb9f946b94c
Henrique Kirch Heck <86362827+henriquekirchheck@users.noreply.github.com>
2024-08-05 17:50:05 -0300

    seg 05 ago 2024 17:50:05 -03

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ export const App = () => {
             <li><a href="/ex01">Desafio 1</a></li>
             <li><a href="/ex02">Desafio 2</a></li>
             <li><a href="/ex03">Desafio 3</a></li>
+            <li><a href="/ex04/">Desafio 4</a></li>
+            <li><a href="/ex05/">Desafio 5</a></li>
         </ul>
     </>
 }
